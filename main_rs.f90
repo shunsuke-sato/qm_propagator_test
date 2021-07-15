f4948db7986c862b06ec0e918e38b5e8c9f0113f
Shunsuke Sato <shunsuke.sato26@gmail.com>
2021-07-15 09:41:57 +0200

    Updated.

--- main_rs.f90
+++ main_rs.f90
@@ -6,12 +6,15 @@ module global_variables
 
 ! grids
   integer :: nx
+  real(8) :: length_x, dx
 
 ! quantum system
+  real(8),allocatable :: psi(:)
   complex(8),allocatable :: zpsi(:)
 
 ! hamiltonian, potential, etc
-  complex(8),allocatable :: zham(:,:),zham0(:,:),zvpot(:,:)
+  real(8),allocatable :: vpot(:), ham(:,:)
+  complex(8),allocatable :: zham(:,:)
 
 ! finite difference coefficients
   real(8) :: c0,c1,c2,g1,g2
@@ -38,45 +41,64 @@ end program main
 subroutine preparation
   use global_variables
   implicit none
-  integer :: ix,jx, k
-  real(8) :: hal2, hal3, hal5, hal7
+  integer :: ix,jx
   real(8) :: xx, ff
-  real(8) :: x1,x2
-  complex(8),allocatable :: za(:,:), zb(:,:)
 
-  nx = 512
+  length_x = 10d0
+  nx = 256
+
+  dx = length_x/nx
+
+! finite difference coefficient
+  c0 = -5d0/2d0/dx**2
+  c1 =  4d0/3d0/dx**2
+  c2 = -1d0/12d0/dx**2
+
+  g1 = 2d0/3d0/dx
+  g2 = -1d0/12d0/dx
+
+  allocate(psi(nx),zpsi(nx))
+  allocate(vpot(nx), ham(nx,nx), zham(nx,nx))
 
 
-  allocate(zpsi(nx))
-  allocate(zham(nx,nx), zham0(nx,nx), zvpot(nx,nx))
-  allocate(za(nx,nx), zb(nx,nx))
-  za = 0d0
-  zb = 0d0
-
-  do jx = 1, nx
-    do ix = 1, nx
-      k = ix + (jx-1)*nx
-      call vdCorput_sequence(k,2,hal2)
-      call vdCorput_sequence(k,3,hal3)
-      call vdCorput_sequence(k,5,hal5)
-      call vdCorput_sequence(k,7,hal7)
-      x1 = sqrt(-2d0*log(hal2))*cos(2d0*pi*hal3)
-      x2 = sqrt(-2d0*log(hal2))*sin(2d0*pi*hal3)
-!      za(ix,jx) = x1 !hal2+zi*hal3 !*exp(zi*2d0*pi*hal3)
-!      zb(ix,jx) = zi*x2 !hal5+zi*hal7 !*exp(zi*2d0*pi*hal7)
-
-      za(ix,jx) = 1d0/(1d0+dble(abs(ix-jx))**2)
-      if(ix /= jx)zb(ix,jx) = zi/dble(ix-jx)**3
-    end do
+  do ix = 1,nx
+    xx = dx*ix
+    vpot(ix) = 0.1d0*sin(pi*xx/length_x)**2
+!    vpot(ix) = 1d0/(pi)*sin(pi*sin(2d0*pi*xx/length_x))
+!    call random_number(ff)
+!    vpot(ix) = ff
   end do
 
-!  zham0 = 0.5d0*(za + transpose(conjg(za)) )
-!  zvpot = 0.5d0*(zb + transpose(conjg(zb)) )
 
-  zham0 = za
-  zvpot = zb
+  ham = 0d0
+  do ix = 1, nx
 
-!  write(*,*)"hel",sum(abs(zham0 - transpose(conjg(zham0)))),sum(abs(zvpot - transpose(conjg(zvpot))))
+! +2
+    jx = ix + 2 +nx-1
+    jx = mod(jx, nx) + 1
+    ham(ix,jx) = -0.5d0*c2
+
+! +1
+    jx = ix + 1 +nx-1
+    jx = mod(jx, nx) + 1
+    ham(ix,jx) = -0.5d0*c1
+
+! +/- 0
+    jx = ix + 0 +nx-1
+    jx = mod(jx, nx) + 1
+    ham(ix,jx) = -0.5d0*c0 + vpot(ix)
+
+! -1
+    jx = ix - 1 +nx-1
+    jx = mod(jx, nx) + 1
+    ham(ix,jx) = -0.5d0*c1
+
+! -2
+    jx = ix - 2 +nx-1
+    jx = mod(jx, nx) + 1
+    ham(ix,jx) = -0.5d0*c2
+
+  end do
 
 end subroutine preparation
 !-------------------------------------------------------------------------------
@@ -86,28 +108,28 @@ subroutine calc_ground_state
   integer :: ix
   real(8) :: ss
 !==LAPACK
-  integer :: nmax
-  integer :: lwork
-  complex(8),allocatable :: za(:,:),work_lp(:)
-  real(8),allocatable :: rwork(:),w(:)
-  integer :: info
+    integer :: nmax
+    integer :: lwork
+    real(8),allocatable :: work_lp(:)
+    real(8),allocatable :: rwork(:),w(:)
+    integer :: info
 
-  nmax = nx
-  lwork = 6*nmax**2
-  allocate(za(nmax,nmax),work_lp(lwork),rwork(3*nmax-2),w(nmax))
+    nmax = nx
+    lwork = 6*nmax**2
+    allocate(work_lp(lwork),rwork(3*nmax-2),w(nmax))
 !==LAPACK
 
-  za = zham0
-  Call zheev('V', 'U', nmax, za, nmax, w, work_lp, lwork, rwork, info)
-!  zpsi(:) = za(:,1)
-  zpsi(:) = za(:,1)
 
-  ss = sum(abs(zpsi)**2)
-  zpsi = zpsi/sqrt(ss)
+  call dsyev('V', 'U', nmax, ham, nmax, w, work_lp, lwork, info)
+  psi(:) = ham(:,1)
+
+  ss = sum(psi**2)*dx
+  psi = psi/sqrt(ss)
 
   open(20,file='gs_wfn_pot.out')
+  write(20,"(999e26.16e3)")0d0, psi(nx), vpot(nx)
   do ix = 1,nx
-    write(20,"(I7,2x,999e26.16e3)")ix, zpsi(ix)
+    write(20,"(999e26.16e3)")ix*dx, psi(ix), vpot(ix)
   end do
   close(20)
 
@@ -116,26 +138,27 @@ end subroutine calc_ground_state
 subroutine calc_time_propagation
   use global_variables
   implicit none
-  real(8):: tt, jt_t, jt_t0
+  real(8):: tt, jt_t
   integer :: it
 
-  Tprop = 10d0
-  dt = 0.5d0
+  zpsi = psi
+
+  Tprop = 2000d0
+  dt = 0.01d0
   nt = Tprop/dt
   call set_field
 
   open(30,file='jt_act.out')
-  call calc_current(zpsi, jt_t)
-  jt_t0 = jt_t
-  write(30,"(999e26.16e3)")0d0, jt_t, act(0),sum(abs(zpsi)**2), jt_t-jt_t0
+  call calc_current(zpsi,act(0), jt_t)
+  write(30,"(999e26.16e3)")0d0, jt_t, act(0),sum(abs(zpsi)**2)*dx
 
 
   do it = 0, nt-1
 
     call dt_evolve(it)
 
-    call calc_current(zpsi, jt_t)
-    write(30,"(999e26.16e3)")(it+1)*dt, jt_t, act(it+1),sum(abs(zpsi)**2), jt_t-jt_t0
+    call calc_current(zpsi,act(it+1), jt_t)
+    write(30,"(999e26.16e3)")(it+1)*dt, jt_t, act(it+1),sum(abs(zpsi)**2)*dx
 
   end do
   close(30)
@@ -154,12 +177,10 @@ subroutine set_field
   do it = 0, nt
     tt = dt*it
     if(tt<Tprop)then
-      act(it) = 1d-8*sin(pi*tt/tprop)**8
-!      act(it) = 1d-6*sin(10*pi*tt/tprop)
+      act(it) = 2d0*pi/length_x*sin(pi*tt/tprop)**8
     end if
   end do
 
-!  act = 1d-8 ! debug
 
 end subroutine set_field
 !-------------------------------------------------------------------------------
@@ -219,7 +240,7 @@ end subroutine taylor_expansion
 subroutine lanczos(act_t, dt_t)
   use global_variables
   implicit none
-  integer,parameter :: n_lanczos = 32
+  integer,parameter :: n_lanczos = 16
   real(8),intent(in) :: act_t, dt_t
   complex(8) :: zvec(nx,n_lanczos)
   complex(8) :: zpsi_t(nx), zhpsi_t(nx)
@@ -239,18 +260,18 @@ subroutine lanczos(act_t, dt_t)
 !  allocate(work_lp(lwork),rwork(3*nmax-2),w(nmax))
 !==LAPACK
 
-  ss = sum(abs(zpsi)**2)
+  ss = sum(abs(zpsi)**2)*dx
   coeff_ini = sqrt(ss)
 
   zvec(:,1) = zpsi/sqrt(ss)
   zpsi_t = zvec(:,1)
   call calc_zhpsi(zpsi_t, zhpsi_t, act_t)
-  alpha_l(1) = sum(conjg(zhpsi_t)*zvec(:,1))
+  alpha_l(1) = sum(conjg(zhpsi_t)*zvec(:,1))*dx
   zpsi_t = zhpsi_t - alpha_l(1)*zvec(:,1)
 
   nmax = n_lanczos
   do j = 2, n_lanczos
-    ss = sum(abs(zpsi_t)**2)
+    ss = sum(abs(zpsi_t)**2)*dx
     beta_l(j) = sqrt(ss)
     if(beta_l(j) == 0d0)then
       write(*,"(A)")"Warning: the Krylov subspace expansion is truncated in the Lanczos method."
@@ -261,13 +282,13 @@ subroutine lanczos(act_t, dt_t)
     zvec(:,j) = zpsi_t/beta_l(j)
     zpsi_t = zvec(:,j)
     call calc_zhpsi(zpsi_t, zhpsi_t, act_t)
-    alpha_l(j) = sum(conjg(zhpsi_t)*zvec(:,j))
+    alpha_l(j) = sum(conjg(zhpsi_t)*zvec(:,j))*dx
     zpsi_t(:) = zhpsi_t -alpha_l(j)*zvec(:,j)-beta_l(j)*zvec(:,j-1)
 
   end do
 
 !  stop ! debug
-  write(*,*)beta_l(2) ! debug
+!  write(*,*)beta_l(2) ! debug
 
   allocate(ham_l(nmax,nmax), zc(nmax))
   ham_l = 0d0
@@ -304,49 +325,108 @@ subroutine calc_zhpsi(zpsi_t, zhpsi_t, act_t)
   complex(8),intent(in) :: zpsi_t(nx)
   complex(8),intent(out) :: zhpsi_t(nx)
   real(8),intent(in) :: act_t
+  real(8) :: c0t,c1t,c2t,g1t,g2t
+  integer :: ix
 
 
-  zham = zham0 + act_t*zvpot
+  c0t = -0.5d0*c0
+  c1t = -0.5d0*c1
+  c2t = -0.5d0*c2
+  g1t = g1*act_t
+  g2t = g2*act_t
 
-  zhpsi_t = matmul(zham, zpsi_t)
+  zhpsi_t = vpot*zpsi_t
+  
+  ix = 1
+  zhpsi_t(ix) = zhpsi_t(ix) +c0t*zpsi_t(ix) &
+                            +c1t*(zpsi_t(ix+1)+zpsi_t(ix-1+nx)) &
+                            +c2t*(zpsi_t(ix+2)+zpsi_t(ix-2+nx)) &
+                            -zi*(&
+                            g1t*(zpsi_t(ix+1)-zpsi_t(ix-1+nx)) &
+                           +g2t*(zpsi_t(ix+2)-zpsi_t(ix-2+nx)))
+
+  ix = 2
+  zhpsi_t(ix) = zhpsi_t(ix) +c0t*zpsi_t(ix) &
+                            +c1t*(zpsi_t(ix+1)+zpsi_t(ix-1)) &
+                            +c2t*(zpsi_t(ix+2)+zpsi_t(ix-2+nx)) &
+                            -zi*(&
+                            g1t*(zpsi_t(ix+1)-zpsi_t(ix-1)) &
+                           +g2t*(zpsi_t(ix+2)-zpsi_t(ix-2+nx)))
+
+  do ix = 1+2, nx-2
+    zhpsi_t(ix) = zhpsi_t(ix) +c0t*zpsi_t(ix) &
+                              +c1t*(zpsi_t(ix+1)+zpsi_t(ix-1)) &
+                              +c2t*(zpsi_t(ix+2)+zpsi_t(ix-2)) &
+                              -zi*(&
+                              g1t*(zpsi_t(ix+1)-zpsi_t(ix-1)) &
+                             +g2t*(zpsi_t(ix+2)-zpsi_t(ix-2)))
+  end do
+  
+  ix = nx-1
+  zhpsi_t(ix) = zhpsi_t(ix) +c0t*zpsi_t(ix) &
+                            +c1t*(zpsi_t(ix+1)+zpsi_t(ix-1)) &
+                            +c2t*(zpsi_t(ix+2-nx)+zpsi_t(ix-2)) &
+                            -zi*(&
+                            g1t*(zpsi_t(ix+1)-zpsi_t(ix-1)) &
+                           +g2t*(zpsi_t(ix+2-nx)-zpsi_t(ix-2)))
+
+  ix = nx
+  zhpsi_t(ix) = zhpsi_t(ix) +c0t*zpsi_t(ix) &
+                            +c1t*(zpsi_t(ix+1-nx)+zpsi_t(ix-1)) &
+                            +c2t*(zpsi_t(ix+2-nx)+zpsi_t(ix-2)) &
+                            -zi*(&
+                            g1t*(zpsi_t(ix+1-nx)-zpsi_t(ix-1)) &
+                           +g2t*(zpsi_t(ix+2-nx)-zpsi_t(ix-2)))
 
 
 end subroutine calc_zhpsi
 !-------------------------------------------------------------------------------
-subroutine calc_current(zpsi_t, jt_t)
+subroutine calc_current(zpsi_t,act_t, jt_t)
   use global_variables
   implicit none
   complex(8),intent(in) :: zpsi_t(nx)
+  real(8),intent(in) :: act_t
   real(8),intent(out) :: jt_t
   complex(8) :: zgpsi_t(nx)
   integer :: ix
 
-  zgpsi_t = matmul(zvpot,zpsi)
-  jt_t = sum(conjg(zpsi_t)*zgpsi_t)
+  zgpsi_t = act_t*zpsi_t
+
+  ix = 1
+  zgpsi_t(ix) = zgpsi_t(ix) -zi*(&
+                            g1*(zpsi_t(ix+1)-zpsi_t(ix-1+nx)) &
+                           +g2*(zpsi_t(ix+2)-zpsi_t(ix-2+nx)))
+
+  ix = 2
+  zgpsi_t(ix) = zgpsi_t(ix) -zi*(&
+                            g1*(zpsi_t(ix+1)-zpsi_t(ix-1)) &
+                           +g2*(zpsi_t(ix+2)-zpsi_t(ix-2+nx)))
+
+
+  do ix = 1+2, nx-2
+    zgpsi_t(ix) = zgpsi_t(ix) -zi*(&
+                            g1*(zpsi_t(ix+1)-zpsi_t(ix-1)) &
+                           +g2*(zpsi_t(ix+2)-zpsi_t(ix-2)))
+
+  end do
+  
+
+  ix = nx-1
+  zgpsi_t(ix) = zgpsi_t(ix) -zi*(&
+                            g1*(zpsi_t(ix+1)-zpsi_t(ix-1)) &
+                           +g2*(zpsi_t(ix+2-nx)-zpsi_t(ix-2)))
+
+  ix = nx
+  zgpsi_t(ix) = zgpsi_t(ix) -zi*(&
+                            g1*(zpsi_t(ix+1-nx)-zpsi_t(ix-1)) &
+                           +g2*(zpsi_t(ix+2-nx)-zpsi_t(ix-2)))
+
+
+  jt_t = sum(conjg(zpsi_t)*zgpsi_t)*dx
 
 
 end subroutine calc_current
 !-------------------------------------------------------------------------------
-!-----------------------------------------
-subroutine vdCorput_sequence(n_in,nbase,vdc_res)
-  implicit none
-  integer,intent(in) :: n_in, nbase
-  real(8),intent(out) :: vdc_res
-  integer :: n
-  real(8) :: bk
-
-  n = n_in
-  bk = 1d0/nbase
-  vdc_res = 0d0
-
-  do while(n>0)
-     vdc_res = vdc_res + mod(n,nbase)*bk
-     n = n/nbase
-     bk = bk/nbase
-     
-  end do
-  
-end subroutine vdCorput_sequence
 !-------------------------------------------------------------------------------
 !-------------------------------------------------------------------------------
 !-------------------------------------------------------------------------------
